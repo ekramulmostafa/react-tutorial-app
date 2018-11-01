9e7544ee35485775c8f09fb2c5d533d1cd07470d
ekramulmostafa <ekramul@bongobd.com>
2018-11-01 11:43:49 +0600

    unit test 3

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -28,4 +28,38 @@ describe('App', () => {
             expect(app.find('.btn').at(0).text()).toEqual('Submit')
         })
     })
+
+    describe('when creating a note', () => {
+        let testNote = 'test this note';
+
+        beforeEach(() => {
+            app.find('FormControl').simulate('change', {
+                target: { value: testNote }
+            })
+        })
+
+        it('update the text in state', () => {
+            expect(app.state().text).toEqual(testNote)
+        })
+
+        describe('submitting the new note', () => {
+            beforeEach(()=>{
+                app.find('.btn').at(0).simulate('click')
+            })
+
+            it('adds the new to state', () => {
+                expect(app.state().notes[0].text).toEqual(testNote)
+            })
+
+            describe('and click the clear button', () => { 
+                beforeEach(() => {
+                    app.find('.btn').at(1).simulate('click')
+                })
+
+                it('clears the notes in state', () => { 
+                    expect(app.state().notes).toEqual([])
+                })
+            })
+        })
+    })
 })
